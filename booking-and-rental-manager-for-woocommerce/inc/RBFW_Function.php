ce7482a8d115b2a7b9c68510b4ed5d59290efd6c
VitaImaging <Jesse@vitaimaging.com>
2025-09-06 22:37:10 -0500

    Add files via upload

---
+++ booking-and-rental-manager-for-woocommerce/inc/RBFW_Function.php
@@ -0,0 +1,172 @@
+<?php
+	if ( ! defined( 'ABSPATH' ) ) {
+		die;
+	} // Cannot access pages directly.
+	if ( ! class_exists( 'RBFW_Function' ) ) {
+		class RBFW_Function {
+
+			public static function get_post_info( $post_id, $key, $default = '' ) {
+				$data = get_post_meta( $post_id, $key, true ) ?: $default;
+				
+				return self::data_sanitize( $data );
+			}
+
+			public static function data_sanitize( $array ) {
+				if ( ! is_array( $array ) ) {
+					return sanitize_text_field( $array );
+				}
+
+				foreach ( $array as $key => $value ) {
+					if ( is_array( $value ) ) {
+						$array[ $key ] = self::data_sanitize( $value );
+					} else {
+						if ( filter_var( $value, FILTER_VALIDATE_EMAIL ) ) {
+							$array[ $key ] = sanitize_email( $value );
+						} elseif ( filter_var( $value, FILTER_VALIDATE_URL ) ) {
+							$array[ $key ] = esc_url_raw( $value );
+						} else {
+							$array[ $key ] = sanitize_text_field( $value );
+						}
+					}
+				}
+
+				return $array;
+			}
+
+
+			//***********Template********************//
+
+			public static function get_all_template() {
+				
+				$template_path = RBFW_Function::get_template_path('single/');
+				$template_path  = glob( $template_path . "*" );
+				
+				foreach ( $template_path as $template_dir ) {
+					if(is_dir($template_dir)){
+						$template_name = preg_replace("/[^a-zA-Z0-9]/", "",(ucfirst(basename($template_dir,''))));
+						$template_lists[ $template_name ] = $template_name.' Template';
+					}
+					
+				}
+				foreach ( $template_lists as $key => $value ) {
+					$templates[ $key ] = $value;
+				}
+				
+				return apply_filters('rbfw_template_list', $templates );
+			}
+
+			public static function get_template_path($path='') {
+				$theme_path = get_stylesheet_directory().'/templates/'.$path;
+				$default_path = RBFW_TEMPLATE_PATH . $path;
+				if (is_dir($theme_path)) {
+					return $theme_path;
+				} elseif (is_dir($default_path)) {
+					return $default_path;
+				} elseif(file_exists($theme_path)){
+					return $theme_path;
+				}elseif(file_exists($default_path)){
+					return $default_path;
+				}else{
+					return $default_path;
+				}
+			}
+
+			public static function get_template_file_url($path=''){
+				$theme_path = get_stylesheet_directory_uri().'/templates/'.$path;
+				$default_path = RBFW_PLUGIN_URL.'/templates/'. $path;
+				if (is_dir($theme_path)) {
+					return $theme_path;
+				} elseif (is_dir($default_path)) {
+					return $default_path;
+				} elseif(file_exists($theme_path)){
+					return $theme_path;
+				}elseif(file_exists($default_path)){
+					return $default_path;
+				}else{
+					return $default_path;
+				}
+			}
+
+			//*******************************//
+			public static function get_thumbnail( $post_id = '', $image_id = '', $size = 'full' ){
+				return self::get_image_url( $post_id, $image_id, $size );
+			}
+
+			public static function get_image_url( $post_id = '', $image_id = '', $size = 'full' ) {
+				if ($post_id) {
+					$image_id = get_post_thumbnail_id($post_id);
+					$image_id = $image_id ?: self::get_post_info($post_id, 'rbfw_list_thumbnail');
+				}
+				return wp_get_attachment_image_url($image_id, $size);
+			}
+
+			//*******************************//
+			public static function get_faq( $rbfw_id ) {
+				return self::get_post_info( $rbfw_id, 'mep_event_faq', array() );
+			}
+
+			//************************//
+			public static function get_taxonomy( $name ) {
+				return get_terms( array( 'taxonomy' => $name, 'hide_empty' => false ) );
+			}
+
+			//************************//
+			public static function get_settings( $key, $option_name, $default = '' ) {
+				$options = get_option( $option_name );
+
+				return self::settings( $options, $key, $default );
+			}
+
+			public static function settings( $options, $key, $default = '' ) {
+				if ( isset( $options[ $key ] ) && $options[ $key ] ) {
+					$default = $options[ $key ];
+				}
+
+				return $default;
+			}
+
+			public static function get_general_settings( $key, $default = '' ) {
+				$options = get_option( 'rbfw_basic_gen_settings' );
+
+				return self::settings( $options, $key, $default );
+			}
+
+			public static function get_style_settings( $key, $default = '' ) {
+				$options = get_option( 'rbfw_basic_style_settings' );
+
+				return self::settings( $options, $key, $default );
+			}
+
+			public static function get_translation_settings( $key, $default = '' ) {
+				$options = get_option( 'rbfw_basic_translation_settings' );
+
+				return self::settings( $options, $key, $default );
+			}
+
+			public static function translation_settings( $key, $default = '' ) {
+				$options = get_option( 'rbfw_basic_translation_settings' );
+				echo esc_html( self::settings( $options, $key, $default ) );
+			}
+
+			//***************************//
+			public static function get_cpt_name(): string {
+				return 'rbfw_item';
+			}
+
+            public static function rbfw_rent_types( ) {
+                $item_type = [
+                    'bike_car_sd'     => __('Rent item for single day', 'booking-and-rental-manager-for-woocommerce'),
+                    'bike_car_md'     => __('Rent item for multiple day', 'booking-and-rental-manager-for-woocommerce'),
+                    'resort'          => __('Resort', 'booking-and-rental-manager-for-woocommerce'),
+                    'equipment'       => __('Equipment', 'booking-and-rental-manager-for-woocommerce'),
+                    'dress'           => __('Dress', 'booking-and-rental-manager-for-woocommerce'),
+                    'appointment'     => __('Appointment', 'booking-and-rental-manager-for-woocommerce'),
+                    'others'          => __('Others', 'booking-and-rental-manager-for-woocommerce'),
+                    'multiple_items'  => __('Multiple day for multiple items', 'booking-and-rental-manager-for-woocommerce'),
+                ];
+                return $item_type;
+            }
+
+			//****************feture lists***********//
+		}
+	}
---
+++ booking-and-rental-manager-for-woocommerce/inc/rbfw_file_include.php
@@ -0,0 +1,100 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
+require_once RBFW_PLUGIN_DIR . '/inc/RBFW_Function.php';
+require_once RBFW_PLUGIN_DIR . '/inc/RBFW_Frontend.php';
+require_once RBFW_PLUGIN_DIR . '/inc/RBFW_Super_Slider.php';
+require_once RBFW_PLUGIN_DIR . '/inc/RBFW_Style.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class.settings-api.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-admin-menu.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-form-fields-generator.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-form-fields-wrapper.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-meta-box.php';
+require_once RBFW_PLUGIN_DIR . '/admin/admin.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-icon-library.php';
+require_once RBFW_PLUGIN_DIR . '/inc/rbfw_functions.php';
+require_once RBFW_PLUGIN_DIR . '/inc/rbfw_inventory_functions.php';
+require_once RBFW_PLUGIN_DIR . '/inc/rbfw_dynamic_css.php';
+require_once RBFW_PLUGIN_DIR . '/inc/class-resort-function.php';
+require_once RBFW_PLUGIN_DIR . '/inc/rbfw_shortcodes.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-pro-page.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-welcome-page.php';
+require_once RBFW_PLUGIN_DIR . '/inc/class-bike-car-sd-function.php';
+require_once RBFW_PLUGIN_DIR . '/inc/rbfw_currency.php';
+
+
+require_once RBFW_PLUGIN_DIR . '/inc/rbfw_order_meta.php';
+require_once RBFW_PLUGIN_DIR . '/inc/class-bike-car-md-function.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-thankyou-page.php';
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-search-page.php';
+
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-rating-notice.php';
+
+require_once RBFW_PLUGIN_DIR . '/lib/classes/class-time-slots-page.php';
+require_once RBFW_PLUGIN_DIR . '/support/elementor/elementor-support.php';
+
+require_once RBFW_PLUGIN_DIR . '/support/blocks/block-support.php';
+//require_once RBFW_PLUGIN_DIR . '/lib/classes/class-quick-setup.php';
+
+
+
+add_action('init', 'rbfw_category_update');
+function rbfw_category_update(){
+    $rbfw_category_update = get_option('rbfw_enable_time_picker_option');
+
+    if($rbfw_category_update != 'yes'){
+
+        $args = array(
+            'post_type' => 'rbfw_item',
+            'posts_per_page' => -1,
+        );
+        $query = new WP_Query($args);
+
+        if($query->have_posts()): while ( $query->have_posts() ) : $query->the_post();
+
+         $enable_specific_duration =  get_post_meta(get_the_ID(), 'enable_specific_duration', true) ? get_post_meta(get_the_ID(), 'enable_specific_duration', true) : 'off';
+            $rbfw_time_slot_switch = !empty(get_post_meta(get_the_ID(),'rbfw_time_slot_switch',true)) ? get_post_meta(get_the_ID(),'rbfw_time_slot_switch',true) : 'off';
+            $available_times = get_post_meta(get_the_ID(), 'rdfw_available_time', true) ? maybe_unserialize(get_post_meta(get_the_ID(), 'rdfw_available_time', true)) : [];
+            $enable_hourly_rate = get_post_meta(get_the_ID(), 'rbfw_enable_hourly_rate', true) ? get_post_meta(get_the_ID(), 'rbfw_enable_hourly_rate', true) : 'no';
+
+            if($rbfw_time_slot_switch == 'on' && !empty($available_times) &&  $enable_specific_duration =='off' ){
+                update_post_meta(get_the_ID(),'rbfw_enable_time_picker','yes');
+            }
+
+            if($rbfw_time_slot_switch == 'on' && !empty($availabe_time) && $enable_hourly_rate == 'yes' ){
+                update_post_meta(get_the_ID(),'rbfw_enable_time_picker','yes');
+            }
+
+            endwhile;
+        endif;
+        update_option( 'rbfw_enable_time_picker_option', 'yes' );
+    }
+}
+
+/*************************************************
+* if Woocommerce Payment System is Enabled
+**************************************************/
+add_action('wp_loaded', 'rbfw_free_woocommerce_integrate');
+
+function rbfw_free_woocommerce_integrate(){
+
+    require_once(RBFW_PLUGIN_DIR . "/inc/woocommerce/rbfw_wc_notice.php");
+    require_once(RBFW_PLUGIN_DIR . "/Frontend/RBFW_Woocommerse.php");
+    require_once(RBFW_PLUGIN_DIR . "/inc/woocommerce/class-status.php");
+    require_once(RBFW_PLUGIN_DIR . "/inc/woocommerce/class-meta.php");
+    require_once(RBFW_PLUGIN_DIR . "/inc/woocommerce/rbfw_cart_price_function.php");
+
+}
+
+
+add_filter('rbfw_payment_systems','rbfw_payment_systems_free', 9);
+
+function rbfw_payment_systems_free(){
+
+    $ps = array(
+        'wps' => 'WC Payment System',
+    );
+
+    return $ps;
+}
+
+
